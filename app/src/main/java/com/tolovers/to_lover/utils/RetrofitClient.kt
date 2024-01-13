b466cbad27a91328279e0e4550f22cba6042462a
Junho Lim <66125576+junderer@users.noreply.github.com>
2024-01-13 10:57:33 +0900

    Revert "FEAT chatting"

--- app/src/main/java/com/tolovers/to_lover/utils/RetrofitClient.kt
+++ app/src/main/java/com/tolovers/to_lover/utils/RetrofitClient.kt
@@ -1,6 +1,5 @@
 package com.tolovers.to_lover.utils
 
-import com.tolovers.to_lover.api.ChatInterface
 import com.tolovers.to_lover.api.UserInterface
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,6 +13,5 @@ object RetrofitClient {
         .build()
 
     val userApi = retrofit.create<UserInterface>(UserInterface::class.java)
-    val chatApi = retrofit.create<ChatInterface>(ChatInterface::class.java)
 
 }
